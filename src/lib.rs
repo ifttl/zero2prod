ab9a7198c6ecb51d24b3cac50619d6a9a025e413
Erik <2408522586@qq.com>
2023-05-06 13:52:01 +0800

    change project structure for easier testing

---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+//! lib.rs
+
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn health_check(_req: HttpRequest) -> impl Responder {
+    // use `HttpResponse::Ok` to get a `HttpResponseBuilder`
+    // primed with a 200 OK status code
+    // `HttpResponseBuilder` implements `Responder`
+    // we can therefore omit our call to `finish`
+    // HttpResponse::Ok().finish()
+    HttpResponse::Ok().finish()
+}
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+/*
+`HttpServer` handles all *transport level* concerns
+
+`App` is the component whose job is to take an incoming request as input
+  and spit out a response
+
+`route` takes two parameters:
+  * path, a string, possibly templated to accomodate dynamic path segments
+  * route, an instance of `Route` struct
+
+`web::get()` is a short-cut for `Route::new().guard(guard::Get())
+*/
+pub async fn run() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,13 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+//! main.rs
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check(_req: HttpRequest) -> impl Responder {
-    // use `HttpResponse::Ok` to get a `HttpResponseBuilder`
-    // primed with a 200 OK status code
-    // `HttpResponseBuilder` implements `Responder`
-    // we can therefore omit our call to `finish`
-    // HttpResponse::Ok().finish()
-    HttpResponse::Ok()
-}
+use zero2prod::run;
 
 /*
 `main` is the entry point of the program, **cannot** be `async`
 `tokio::main` is a macro that sets up a tokio runtime
 use `cargo expand` to see the expanded code
-
-`HttpServer` handles all *transport level* concerns
-
-`App` is the component whose job is to take an incoming request as input
-  and spit out a response
-
-`route` takes two parameters:
-  * path, a string, possibly templated to accomodate dynamic path segments
-  * route, an instance of `Route` struct
-
-`web::get()` is a short-cut for `Route::new().guard(guard::Get())
 */
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    run().await
 }
