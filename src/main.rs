4e7621efa0c5c7ecd838c122d086ff82692ebcca
Erik <2408522586@qq.com>
2023-05-06 16:37:02 +0800

    change comment of cargo expand in main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use zero2prod::run;
 /*
 `main` is the entry point of the program, **cannot** be `async`
 `tokio::main` is a macro that sets up a tokio runtime
-use `cargo expand` to see the expanded code
+use `cargo expand --bin zero2prod` to see the expanded code
 */
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
