9c758e8c22a72e34dbb42147cad02963055eea2a
Erik <2408522586@qq.com>
2023-05-31 10:07:41 +0800

    Preparation and testing of the database have been completed.

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,8 @@
 //! tests/health_check.rs
 
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 
 // `cargo expand --test health_check` (<- name of the test file)
 #[tokio::test]
@@ -36,6 +38,11 @@ fn spawn_app() -> String {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
 
     let body = "name=john%20doe&email=john.doe%40gmail.com";
@@ -48,6 +55,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request.");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "john.doe@gmail.com");
+    assert_eq!(saved.name, "john doe");
 }
 
 #[tokio::test]
