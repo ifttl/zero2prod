e0df6e7cf06b6530d3ff5d2fa9123b3d7d855689
Erik <2408522586@qq.com>
2023-10-20 11:32:54 +0800

    log

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use env_logger::Env;
 
 /*
 `main` is the entry point of the program, **cannot** be `async`
@@ -12,6 +13,7 @@ use `cargo expand --bin zero2prod` to see the expanded code
 */
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
